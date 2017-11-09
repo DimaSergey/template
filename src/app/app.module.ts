0648bfc689f8f1df1ff8124d19fc00069019cad3
dimonluk <lds4ever2000@gmail.com>
2017-11-09 15:57:15 +0200

    Routing is made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from './components/app.component/app.component';
+import { IndexComponent } from './components/index/index.component';
+
+const appRoutes = [
+	{path:"", component:IndexComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		IndexComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot(
+			appRoutes,
+			{enableTracing: true}
+		)
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
